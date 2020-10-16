4bd2c1f4f2d285cce63851c0d68b3cd03c3a1ad7
ia00e1317 <56835708+ia00e1317@users.noreply.github.com>
2020-10-16 23:46:11 +0900

    Update views.py

--- toppage/views.py
+++ toppage/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Article
 
-from django.contrib.auth.decorators import login_required   #追加
-from django.contrib.auth.mixins import LoginRequiredMixin   #追加
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
@@ -12,8 +12,8 @@ from django.views.generic.edit import FormView
 from . import forms
 #from .forms import ArticleForm
 
-import csv   #追加
-from io import TextIOWrapper, StringIO   #追加
+import csv
+from io import TextIOWrapper, StringIO
 
 from .models import SimplePhoto
 
@@ -22,15 +22,6 @@ import shutil
 
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
-
-
-
-#今回はやらないが今後の実装を考える事項
-#登録にメールアドレスを利用する
-#CSSファイルの切り出し
-#一括問い合わせ機能は必要か？まとめて物件情報送ります
-#未完成：画像ファイル削除機能
-#未完成：登録メアドを問い合わせフォームのデフォルト入力
 
 
 def index(request, roomtype='all'):
@@ -47,7 +38,7 @@ def index(request, roomtype='all'):
     else:
         searchForm = forms.SearchForm()
         if roomtype == 'all':
-            articles = Article.objects.all()#.order_by('price').reverse()
+            articles = Article.objects.all()
         elif  roomtype == '2dk':
             articles = Article.objects.filter(planType__contains='2DK')
         else:
@@ -59,8 +50,6 @@ def index(request, roomtype='all'):
     sortValue = ''
     if sortForm.is_valid():
         sortValue = request.GET.get('sort_s', None)
-        #if sortValue[0:1] == "p"
-        #if sortValue[1:2] == "u"
         if sortValue == 'pu':
             articles = articles.order_by('price')
         elif sortValue == 'pd':
@@ -247,7 +236,7 @@ def pictureup(request, id, roomtype):
 
 
 #問い合わせフォーム
-class ContactFormView(LoginRequiredMixin,FormView): #BaseFormView
+class ContactFormView(LoginRequiredMixin,FormView):
     model = Article
     template_name = 'contact/contact_form.html'
     form_class = forms.ContactForm
@@ -256,19 +245,16 @@ class ContactFormView(LoginRequiredMixin,FormView): #BaseFormView
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         #context["datas"] = Article.objects.all()
-        id = self.kwargs.get('id')   #
+        id = self.kwargs.get('id')
         context["article"] = Article.objects.get(pk=id)
         #user = self.request.user
         #context["user"] = user
         return context
 
-    #def form_valid(self, form):
-    #def form_valid(self, form, id):
     def form_valid(self, form, **kwargs):
         #
         id = self.kwargs.get('id')
-        article = Article.objects.get(pk=id)#1797
-        #form.send_email()
+        article = Article.objects.get(pk=id)
         form.send_email(article)
         return super().form_valid(form)
 
@@ -292,36 +278,31 @@ def upload(request):
         Article.objects.all().delete()
 
         form_data = TextIOWrapper(request.FILES['csv'].file, encoding='ANSI')
-#        form_data = TextIOWrapper(request.FILES['csv'].file, encoding='utf-8')
         csv_file = csv.reader(form_data)
         for i,line in enumerate(csv_file):
-            #article, created = Articleup.objects.get_or_create(no=line[0])
-            #article = Article(no=line[0])
 
             if i == 0:
                 continue
 
             article = Article()
-            article.bknNumber = line[0]#
-            article.bknName = line[18]#
+            article.bknNumber = line[0]
+            article.bknName = line[18]
 
             if line[46] != '':
                 linestr = line[46]
-                article.price = "{:,}".format(int(linestr[0:len(linestr)-4]))#
+                article.price = "{:,}".format(int(linestr[0:len(linestr)-4]))
 
             if line[48] != '':
                 linestr = line[48]
                 integerPrt = "{:,}".format(int(linestr[0:len(linestr)-4]))
                 decimalPrt = linestr[len(linestr)-4:len(linestr)-3]
                 article.priceTsubo = integerPrt + '.' + decimalPrt
-                #article.priceTsubo = "{:,}".format(int(linestr[0:len(linestr)-4]))#★★★
 
             if line[49] != '':
                 linestr = line[49]
                 integerPrt = "{:,}".format(int(linestr[0:len(linestr)-4]))
                 decimalPrt = linestr[len(linestr)-4:len(linestr)-3]
                 article.priceSquare = integerPrt + '.' + decimalPrt
-                #article.priceSquare = "{:,}".format(int(linestr[0:len(linestr)-4]))#★★★
 
             if line[133] and line[132]:
                 lst = ['dm0', 'dm1', 'dm2', 'DK']
@@ -330,21 +311,20 @@ def upload(request):
                     article.planType = line[133] + lst[idx]
                 else:
                     article.planType = '???'
-            #article.planType = line[133] + '★' + line[132] + '★'#
 
-            article.planRoom = line[164]#
-            article.address1 = line[14] + line[15]#
-            article.address2 = line[16]#
-            article.railwayLine = line[22]#
-            article.station1 = line[23]#
-            article.walkMin1 = line[24]#★★★
-            article.walkMet1 = line[25]#★★★
-            article.station2 = line[32]#####
-            article.walkMin2 = line[33]#★★★
-            article.walkMet2 = line[34]#★★★
-            article.environment = line[189]#
-            article.environmentMet = line[190]#★★★
-            article.environmentMin = line[191]#★★★
+            article.planRoom = line[164]
+            article.address1 = line[14] + line[15]
+            article.address2 = line[16]
+            article.railwayLine = line[22]
+            article.station1 = line[23]
+            article.walkMin1 = line[24]
+            article.walkMet1 = line[25]
+            article.station2 = line[32]
+            article.walkMin2 = line[33]
+            article.walkMet2 = line[34]
+            article.environment = line[189]
+            article.environmentMet = line[190]
+            article.environmentMin = line[191]
 
             if line[35]:
                 lst = ['dm0', '居住中', '空家', '賃貸中']
@@ -365,8 +345,8 @@ def upload(request):
             #article.bknType = line[3]
 
             article.bknTrade = line[2]
-            article.exclusiveArea = line[57]#★★★
-            article.balArea = line[60]#★★★
+            article.exclusiveArea = line[57]
+            article.balArea = line[60]
             
             if line[97]:
                 lst = ['dm0', '有', '無']
@@ -387,10 +367,10 @@ def upload(request):
             #article.associationType = line[98]
 
             if line[101] != '':
-                article.administrative = "{:,}".format(int(line[101]))#★★★
+                article.administrative = "{:,}".format(int(line[101]))
 
             if line[103] != '':
-                article.repairReserve = "{:,}".format(int(line[103]))#★★★
+                article.repairReserve = "{:,}".format(int(line[103]))
 
             if line[172]:
                 lst = ['dm0', 'dm1', 'dm2', '鉄骨造', 'ＲＣ', 'ＳＲＣ', 'dm6', 'dm7', '軽量鉄骨', 'その他']
@@ -401,7 +381,7 @@ def upload(request):
                     article.structure = '???'
             #article.structure = line[172]
 
-            article.stairs = line[177] + '／' + line[175]#
+            article.stairs = line[177] + '／' + line[175]
 
             if line[182]:
                 lst = ['dm0', '北', '北東', '東', '南東', '南', '南西', '西', '北西']
@@ -413,24 +393,23 @@ def upload(request):
             #article.balDirection = line[182]
 
             linestr = line[178]
-            article.constructionDate = linestr[0:4] + '年' + linestr[4:6] + '月'#
+            article.constructionDate = linestr[0:4] + '年' + linestr[4:6] + '月'
 
             linestr = line[183]
-            article.extensionDate1 = linestr[0:4] + '年' + linestr[4:6] + '月'#
+            article.extensionDate1 = linestr[0:4] + '年' + linestr[4:6] + '月'
 
             article.extensionHistory1 = line[184]
 
             linestr = line[185]
-            article.extensionDate2 = linestr[0:4] + '年' + linestr[4:6] + '月'#
+            article.extensionDate2 = linestr[0:4] + '年' + linestr[4:6] + '月'
 
             article.extensionHistory2 = line[186]
 
             linestr = line[187]
-            article.extensionDate3 = linestr[0:4] + '年' + linestr[4:6] + '月'#
+            article.extensionDate3 = linestr[0:4] + '年' + linestr[4:6] + '月'
             
             article.extensionHistory3 = line[188]
             article.pictureFlag = "写真なし"#line[197]
-            #article.soldout = 
 
             article.save()
 
